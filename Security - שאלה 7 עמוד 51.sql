9d7c17a58e0df29fc0a4e82cdc2153c6fbd24aaa
NachshonT <46367479+NachshonT@users.noreply.github.com>
2019-01-17 22:02:20 +0200

    Add files via upload

---
+++ Security - שאלה 7 עמוד 51.sql
@@ -0,0 +1,18 @@
+TITLE: Connect to Server
+------------------------------
+
+Cannot connect to TAMIR1\TAMIR1SQLEXPRESS.
+
+------------------------------
+ADDITIONAL INFORMATION:
+
+Cannot open user default database. Login failed.
+Login failed for user 'Bob'. (Microsoft SQL Server, Error: 4064)
+
+For help, click: http://go.microsoft.com/fwlink?ProdName=Microsoft%20SQL%20Server&EvtSrc=MSSQLServer&EvtID=4064&LinkId=20476
+
+------------------------------
+BUTTONS:
+
+OK
+------------------------------
